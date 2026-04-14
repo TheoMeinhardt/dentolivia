4e084f85531bb63ebf3fdff2704854036c2f76d3
Theo Meinhardt <theo.meinhardt@batinfo.at>
2026-04-14 11:10:35 +0200

    Added text and animations to splashscreen

--- src/components/Splashscreen.vue
+++ src/components/Splashscreen.vue
@@ -1,20 +1,46 @@
 <template>
   <div v-show="isVisible" @click="hideScreen()" class="splashscreen">
-    <h1>Hallo</h1>
+    <div class="text-center full-width full-height ">
+
+      <div class="fixed-center text-secondary">
+
+        <q-img id="animationTarget" src="@/assets/icons/Logo.svg" width="15rem" class="hide" />
+
+        <span id="animationTarget" class="text-h3 q-mt-sm text-bold block hide ">Zahnmedizin, die zu Ihnen
+          passt!</span>
+        <span id="animationTarget" class="text-h4 q-mt-md border-seperator font-TradeGothic block hide">Zeit für klare
+          Beratungsgespräche
+          und gemeinsame Entscheidungen</span>
+        <span id="animationTarget" class="text-h5 text-italic q-mt-lg block hide">Dr. med. dent.</span>
+        <span id="animationTarget" class="text-h1 block q-mb-xl hide">Isabelle Olivia</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import type { Ref } from "vue"
-import { ref } from "vue"
+import { onMounted, ref } from "vue"
 
 let isVisible: Ref<boolean> = ref(true)
+let animationTargets
 
 // disable scroll
 document.body.style.overflow = "hidden"
 
+onMounted(() => {
+  animationTargets = document.querySelectorAll("#animationTarget")
+
+  animationTargets.forEach((item, i) => {
+    setTimeout(() => {
+      item.classList.replace("hide", "show")
+    }, i * 750)
+  })
+})
+
 function hideScreen() {
   isVisible.value = false
+  document.body.style.overflow = "visible"
 }
 </script>
 
@@ -28,5 +54,27 @@ function hideScreen() {
   z-index: 10;
 
   background-color: white;
+}
+
+@keyframes ease-opacity {
+  from {
+    opacity: 0%;
+  }
+
+  to {
+    opacity: 100%;
+  }
+}
+
+.hide {
+  opacity: 0%;
+}
+
+.show {
+  animation: ease-opacity .75s ease-in 0s 1 normal forwards;
+}
+
+.border-seperator {
+  border-bottom: 5px solid $primary;
 }
 </style>
